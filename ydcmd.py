24c0381a12788ce27005a7da6007fcbe685b30d4
Anton Batenev <antonbatenev@yandex.ru>
2014-07-10 01:56:14 +0400

    RC

--- ydcmd.py
+++ ydcmd.py
@@ -7,7 +7,7 @@ __author__   = "Anton Batenev"
 __license__  = "BSD"
 
 
-__all__ = ["ydError", "ydCertError", "ydConfig", "ydOptions", "ydBase", "ydCmd"]
+__all__ = ["ydError", "ydCertError", "ydConfig", "ydOptions", "ydBase", "ydExtended", "ydCmd"]
 
 
 import array, os, sys, time
