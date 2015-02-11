06a54e9638b7c1bc2878d5f2a8861c4de2983095
Anton Batenev <antonbatenev@yandex.ru>
2015-02-11 03:48:16 +0300

    Удаление ссылок на Windows - не тестируется, не поддерживается

--- windows/setup.py
+++ windows/setup.py
@@ -16,7 +16,7 @@ build_exe_options = {
 
 setup (
     name        = "ydcmd",
-    version     = "1.4",
+    version     = "1.5",
     description = "Command line client for Yandex.Disk",
     options     = { "build_exe": build_exe_options },
     executables = [ Executable("ydcmd.py", base = None) ]
